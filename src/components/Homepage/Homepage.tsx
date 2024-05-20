5c15d390a4957d39779e1895e0a5b22caa1ee8e1
aloan93 <a.loan93@gmail.com>
2024-05-20 20:48:05 +0100

    Homepage component started - routes and route implemented - small styling changes to app

---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+import styles from "./Homepage.module.css";
+
+export default function Homepage(): ReactNode {
+  return <div className={styles.homepage}></div>;
+}
