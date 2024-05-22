56c1980949ecc1c9a74f5c2f06e7e0258c0f5031
aloan93 <a.loan93@gmail.com>
2024-05-22 22:19:37 +0100

    added loading and error states to homepage

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -6,27 +6,37 @@ import SearchBar from "../SearchBar/SearchBar";
 
 export default function Homepage(): ReactNode {
   const [searchResults, setSearchResults] = useState<number[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get("query") || "";
 
   useEffect(() => {
-    if (query)
+    if (query) {
+      setIsLoading(true);
+      setError("");
       metMuseumAPI
         .get(`/search?q=${query}`)
         .then(({ data: { total, objectIDs } }) => {
-          setSearchResults(objectIDs);
+          !objectIDs ? setSearchResults([]) : setSearchResults(objectIDs);
+          setIsLoading(false);
           console.log(total);
         })
         .catch((err) => {
+          setError("Something went wrong. Please try again later.");
+          setIsLoading(false);
           console.log(err);
         });
+    }
   }, [searchParams]);
 
   return (
     <div className={styles.homepage}>
       <SearchBar setSearchParams={setSearchParams} />
+      {isLoading ? <p>Loading...</p> : null}
+      {error ? <p>{error}</p> : null}
       {searchResults.map((id) => {
-        return <p>{id}</p>;
+        return <p key={id}>{id}</p>;
       })}
     </div>
   );
