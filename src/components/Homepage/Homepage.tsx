5de6be5131f33cce1c0f1ab586a8c3cc10af775f
aloan93 <a.loan93@gmail.com>
2024-05-23 17:31:31 +0100

    homepage card now calls to api and gets artifact image - pagination implemented

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -14,13 +14,17 @@ export default function Homepage(): ReactNode {
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get("query") || "";
   const page = searchParams.get("page") || "1";
+  const paginatedSearchResults = searchResults.slice(
+    Number(page) * 20 - 20,
+    Number(page) * 20
+  );
 
   useEffect(() => {
     if (query) {
       setIsLoading(true);
       setError("");
       metMuseumAPI
-        .get(`/search?q=${query}`)
+        .get(`/search?q=${query}&hasImages=true`)
         .then(({ data: { total, objectIDs } }) => {
           !objectIDs ? setSearchResults([]) : setSearchResults(objectIDs);
           setSearchResultsTotal(total);
@@ -32,19 +36,21 @@ export default function Homepage(): ReactNode {
           console.log(err);
         });
     }
-  }, [searchParams]);
+  }, [query]);
 
   return (
     <div className={styles.container}>
       <SearchBar setSearchParams={setSearchParams} />
       {isLoading ? <p>Loading...</p> : null}
       {error ? <p>{error}</p> : null}
-      <PageNav
-        page={page}
-        setSearchParams={setSearchParams}
-        searchResultsTotal={searchResultsTotal}
-      />
-      {searchResults.map((id) => {
+      {searchResults.length > 0 ? (
+        <PageNav
+          page={page}
+          setSearchParams={setSearchParams}
+          searchResultsTotal={searchResultsTotal}
+        />
+      ) : null}
+      {paginatedSearchResults.map((id) => {
         return <HomepageCard key={id} id={id} />;
       })}
     </div>
--- src/components/HomepageCard/HomepageCard.tsx
+++ src/components/HomepageCard/HomepageCard.tsx
@@ -1,6 +1,25 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import styles from "./HomepageCard.module.css";
+import { metMuseumAPI } from "../../api/api";
 
 export default function HomepageCard(props: { id: number }): ReactNode {
-  return <div className={styles.container}></div>;
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    metMuseumAPI
+      .get(`/objects/${props.id}`)
+      .then(({ data: { primaryImageSmall } }) => {
+        setImage(primaryImageSmall);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <p>{props.id}</p>
+      <img src={image} alt="Small image of artwork" />
+    </div>
+  );
 }
