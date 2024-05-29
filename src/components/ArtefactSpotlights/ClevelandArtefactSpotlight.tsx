279e4b6c078ed8451bc75ffc56aaee1ed71f2aa1
aloan93 <a.loan93@gmail.com>
2024-05-29 22:27:35 +0100

    elements laid out with cleveland spotlight with basic styling - new utility function to capitalise strings

--- src/components/ArtefactSpotlights/ClevelandArtefactSpotlight.tsx
+++ src/components/ArtefactSpotlights/ClevelandArtefactSpotlight.tsx
@@ -1,6 +1,12 @@
 import { ReactNode, useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import { clevelandMuseumAPI } from "../../api/api";
+import styles from "./ArtefactSpotlights.module.css";
+import {
+  convertYearToBcOrNot,
+  capitaliseString,
+  getImageURL,
+} from "../../utils";
 
 export default function ClevelandArtefactSpotlight(): ReactNode {
   const { id } = useParams();
@@ -33,11 +39,88 @@ export default function ClevelandArtefactSpotlight(): ReactNode {
     }
   }, []);
 
-  if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
   return (
-    <div>
-      <img src={artefact.images.web.url} alt="" />
+    <div className={styles.container}>
+      {isLoading ? (
+        <div className={styles.loader}></div>
+      ) : (
+        <>
+          <div className={styles.imagesContainer}>
+            {artefact.images.web.url ? (
+              <>
+                <img
+                  className={styles.mainImage}
+                  src={artefact.images.web.url}
+                  alt="Image of the artefact"
+                />
+
+                {artefact.alternate_images
+                  ? artefact.alternate_images.map((image: any) => {
+                      return (
+                        <img
+                          className={styles.alternateImage}
+                          src={image.web.url}
+                          alt=""
+                        />
+                      );
+                    })
+                  : null}
+              </>
+            ) : (
+              <img
+                className={styles.placeholderImage}
+                src={getImageURL("/placeholder.placeholder.png")}
+                alt="PLaceholder image"
+              />
+            )}
+          </div>
+
+          <div className={styles.detailsContainer}>
+            <h2 className={styles.title}>{artefact.title}</h2>
+
+            {artefact.creation_date_earliest ? (
+              <p className={styles.details}>{`${convertYearToBcOrNot(
+                artefact.creation_date_earliest
+              )} - ${convertYearToBcOrNot(artefact.creation_date_latest)}`}</p>
+            ) : null}
+
+            {artefact.culture[0] ? (
+              <p className={styles.details}>{artefact.culture[0]}</p>
+            ) : null}
+
+            <p className={styles.details}>{`${
+              artefact.department || "Dept. unknown"
+            } - ${artefact.type || "Misc."}`}</p>
+
+            {artefact.technique ? (
+              <p className={styles.details}>
+                {capitaliseString(artefact.technique)}
+              </p>
+            ) : null}
+
+            {artefact.creators[0] ? (
+              <p className={styles.details}>
+                {artefact.creators[0].description}
+              </p>
+            ) : null}
+
+            {artefact.description ? (
+              <p className={styles.details}>{artefact.description}</p>
+            ) : null}
+
+            {artefact.did_you_know ? (
+              <p className={styles.details}>{artefact.did_you_know}</p>
+            ) : null}
+
+            {artefact.url ? (
+              <Link to={artefact.url}>
+                <p className={styles.details}>{artefact.url}</p>
+              </Link>
+            ) : null}
+          </div>
+        </>
+      )}
     </div>
   );
 }
