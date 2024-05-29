f6477783865891ac3bc502c7275626643165d832
aloan93 <a.loan93@gmail.com>
2024-05-29 17:53:38 +0100

    cleveland spotlight now calls to api if url is accessed directly so it can still populate data

--- src/components/ArtefactSpotlights/ClevelandArtefactSpotlight.tsx
+++ src/components/ArtefactSpotlights/ClevelandArtefactSpotlight.tsx
@@ -1,12 +1,40 @@
-import { ReactNode } from "react";
-import { useLocation } from "react-router-dom";
+import { ReactNode, useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { clevelandMuseumAPI } from "../../api/api";
 
 export default function ClevelandArtefactSpotlight(): ReactNode {
+  const { id } = useParams();
   const location = useLocation();
-  const artefact = location.state;
+  const [artefact, setArtefact] = useState(location.state);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
-  // logic to call to the api should artefact be null
+  useEffect(() => {
+    if (!artefact) {
+      setIsLoading(true);
+      setError("");
+      clevelandMuseumAPI
+        .get(`/artworks/${id}`)
+        .then(({ data: { data } }) => {
+          setArtefact(data);
+          setIsLoading(false);
+        })
+        .catch(({ response: { status } }) => {
+          status === 404
+            ? setError(
+                "artefact not found! This entry may have been removed from the collection."
+              )
+            : setError("Something went wrong! Please try again later.");
+          setIsLoading(false);
+        });
+    } else {
+      setError("");
+      setIsLoading(false);
+    }
+  }, []);
 
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <div>
       <img src={artefact.images.web.url} alt="" />
