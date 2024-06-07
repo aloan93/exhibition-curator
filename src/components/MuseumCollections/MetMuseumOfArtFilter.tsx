37ec05bf0605ce882ecfd9adc9e2f8f1353633cd
aloan93 <a.loan93@gmail.com>
2024-06-07 17:50:05 +0100

    refactor to metmuseumcard to be more in line with cleveland - middleman component created to handle api call before passing full artefact - should now be able to scrap seperate entry components and just reuse card components within exhbition - this will simplify accessibility refactors

---
+++ src/components/MuseumCollections/MetMuseumOfArtFilter.tsx
@@ -0,0 +1,38 @@
+import { ReactNode, useEffect, useState } from "react";
+import { metMuseumAPI } from "../../api/api";
+import styles from "./MuseumCollections.module.css";
+import MetMuseumOfArtCard from "../MuseumCollectionCards/MetMuseumOfArtCard";
+
+export default function MetMuseumOfArtFilter(props: { id: number }): ReactNode {
+  const [artefact, setArtefact] = useState<any>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError("");
+    metMuseumAPI
+      .get(`/objects/${props.id}`)
+      .then(({ data }) => {
+        setArtefact(data);
+        setIsLoading(false);
+      })
+      .catch(({ response: { status } }) => {
+        status === 404
+          ? setError(
+              "Artefact not found! This entry may have been removed from the collection."
+            )
+          : setError("Something went wrong! Please try again later.");
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <div className={styles.loader}></div>;
+  if (error)
+    return (
+      <div className={styles.errorContainer}>
+        <p className={styles.notFound}>{error}</p>
+      </div>
+    );
+  return <MetMuseumOfArtCard artefact={artefact} />;
+}
