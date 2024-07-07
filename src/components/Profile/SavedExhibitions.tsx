d2beea242806c891e2ed501bc9762bbb69fc2f9b
aloan93 <a.loan93@gmail.com>
2024-07-07 23:26:12 +0100

    added loading and error logic to savedexhibitions component within profile

--- src/components/Profile/SavedExhibitions.tsx
+++ src/components/Profile/SavedExhibitions.tsx
@@ -9,8 +9,12 @@ export default function SavedExhibitions(props: { uid: string }): ReactNode {
   const exhibitionsRef = collection(db, "Exhibitions");
   const userRef = doc(db, "Users", props.uid);
   const q = query(exhibitionsRef, where("user", "==", userRef));
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
+    setIsLoading(true);
+    setError("");
     getDocs(q)
       .then(({ docs }) => {
         const newData = docs.map((doc) => ({
@@ -18,31 +22,50 @@ export default function SavedExhibitions(props: { uid: string }): ReactNode {
           exhibitionId: doc.id,
         }));
         setUserExhibitions(newData);
+        setIsLoading(false);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch(() => {
+        setError("Failed to retreive exhibition. Please try again later");
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <div className={styles.exhibitionsContainer}>
-      {userExhibitions.length > 0 ? (
+      <h2
+        className={
+          styles.exhibitionsTitle
+        }>{`Saved Exhibitions (${userExhibitions.length}/3)`}</h2>
+      {isLoading ? (
+        <div className={styles.loader}></div>
+      ) : (
         <>
-          <h2 className={styles.exhibitionsTitle}>Saved Exhibitions</h2>
-          <ul className={styles.exhibitionsList}>
-            {userExhibitions.map((exhibition, id) => {
-              return (
-                <li key={id} className={styles.exhibitionItem}>
-                  <h3 className={styles.exhibitionTitle}>
-                    <Link
-                      to={`/profile/${exhibition.exhibitionId}`}>{`${exhibition.exhibitionName} (${exhibition.artefacts.length})`}</Link>
-                  </h3>
-                </li>
-              );
-            })}
-          </ul>
+          {error ? (
+            <p className={styles.error}>{error}</p>
+          ) : (
+            <>
+              {userExhibitions.length > 0 ? (
+                <ul className={styles.exhibitionsList}>
+                  {userExhibitions.map((exhibition, id) => {
+                    return (
+                      <li key={id} className={styles.exhibitionItem}>
+                        <h3 className={styles.exhibitionTitle}>
+                          <Link
+                            to={`/profile/${exhibition.exhibitionId}`}>{`${exhibition.exhibitionName} (${exhibition.artefacts.length})`}</Link>
+                        </h3>
+                      </li>
+                    );
+                  })}
+                </ul>
+              ) : (
+                <p className={styles.prompt}>
+                  ...would be here, if you had any
+                </p>
+              )}
+            </>
+          )}
         </>
-      ) : null}
+      )}
     </div>
   );
 }
